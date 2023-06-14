c4db85b8180f9a1f17f7e51629c6089d34b739eb
Bella Gerken <bellagerkenn@gmail.com>
2023-06-14 14:09:24 -0400

    complete get request lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,10 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(url)
+        return response.content
 
     def load_json(self):
-        pass
+        json_content = json.loads(GetRequester.get_response_body(self))
+        return json_content
